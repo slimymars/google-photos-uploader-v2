2783e61defafccce196f538b5cc73074be32d680
slimymars <slimymars@gmail.com>
2019-02-17 15:25:38 +0900

    アルバム選択のUI調整

--- src/google-photos-album.vue
+++ src/google-photos-album.vue
@@ -1,11 +1,12 @@
 <template>
     <div id="google_photos">
-        <button @click="onClick">Get Album List</button>
-        <ul>
-            <li v-for="album in albumList" :id="album.id">{{album.title}}
-                <button @click="addBtn(album.id, album.title)">Add</button>
-            </li>
-        </ul>
+        <button @click="onClick" v-bind:disabled="albumBtnDisabled">{{ albumBtnMsg }}</button>
+        <label for="album-selector">アルバムリスト：</label>
+        <select v-model="selected" v-bind:disabled="albumListDisabled" id="album-selector">
+            <option disabled :value="''">{{ msg }}</option>
+            <option v-for="album in albumList" :value="{id: album.title, title: album.title}">{{ album.title}}</option>
+        </select>
+        <button @click="addBtn" v-bind:disabled="addBtnDisabled">追加</button>
     </div>
 </template>
 
@@ -23,6 +24,12 @@
     @Component
     export default class GooglePhotosAlbumList extends Vue {
         public albumList: { id: string; title: string }[] = [];
+        public msg = "アルバムリスト取得ボタンで取得してください";
+        public selected: { id: string; title: string } | string = "";
+        albumBtnDisabled = false;
+        addBtnDisabled = true;
+        albumListDisabled = true;
+        albumBtnMsg = "アルバムリスト取得";
 
         private getToken(): Promise<string> {
             // noinspection TypeScriptValidateTypes
@@ -44,6 +51,11 @@
         }
 
         public async onClick() {
+            this.albumBtnDisabled = true;
+            this.albumBtnMsg = "取得中";
+            this.selected = '';
+            this.albumListDisabled = true;
+            this.addBtnDisabled = true;
             this.albumList = [];
             const token = await this.getToken();
             let nextPageToken = '';
@@ -53,10 +65,19 @@
                 json_data.albums.forEach(value => this.albumList.push({id: value.id, title: value.title}));
                 nextPageToken = json_data.hasOwnProperty('nextPageToken') ? json_data.nextPageToken : '';
             } while (nextPageToken !== '');
+            this.albumBtnMsg = "アルバムリスト再取得";
+            this.albumBtnDisabled = false;
+            this.albumListDisabled = false;
+            this.addBtnDisabled = false;
+            this.msg = "追加するアルバムを選択してください";
         }
 
-        public addBtn(id: string, title: string) {
-            this.$emit('add-btn', {id: id, title: title})
+        public addBtn() {
+            if (typeof this.selected === 'string') {
+                console.log("why call?")
+            } else {
+                this.$emit('add-btn', this.selected)
+            }
         }
 
     }

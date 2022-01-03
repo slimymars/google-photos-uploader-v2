5299a163f0b259b151f388d7220cc3c8a83b87ac
slimymars <slimymars@gmail.com>
2022-01-03 17:09:50 +0900

    リファラーを送信するよう変更したつもりが実際にはうまくいっていない

--- src/background.ts
+++ src/background.ts
@@ -2,13 +2,24 @@ import {ChromeMenu} from "./chrome_menu";
 import {GooglePhotos} from "./google-photos";
 
 function getImage(imageUrl: string, referrerUrl?: string): Promise<Blob> {
-    return fetch(imageUrl, {
-        credentials: 'include',
-        redirect: "follow",
-        referrer: referrerUrl,
-        referrerPolicy: "unsafe-url"
-
-    }).then((resp) => {
+    let options: RequestInit;
+    if (referrerUrl === undefined) {
+        options = {
+            credentials: 'include',
+            redirect: "follow",
+        }
+    } else {
+        options = {
+            credentials: 'include',
+            redirect: "follow",
+            headers: {
+                'Referer': referrerUrl
+            },
+            referrerPolicy: 'unsafe-url'
+        }
+    }
+    console.log('options', options);
+    return fetch(imageUrl, options).then((resp) => {
         if (resp.ok) return resp.blob();
         resp.text().then((err) => console.log('image get err body text', err));
         throw resp
